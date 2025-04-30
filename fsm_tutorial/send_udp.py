bf8b8e9430e9d428e12d76f60376fd62de842a03
pitsieben <87469485+pitsieben@users.noreply.github.com>
2025-04-30 15:57:16 +0200

    Update send_udp.py

--- fsm_tutorial/send_udp.py
+++ fsm_tutorial/send_udp.py
@@ -1,1 +1,20 @@
+import socket
+
+# === CONFIG ===
+UDP_IP = "192.168.10.120"
 UDP_PORT_GH = 5060
+SEND_RATE = 1  # seconds
+
+# === SETUP SOCKET ===
+sock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+i = 0
+ # Send FSM state to Grasshopper
+while i<1:
+    # Send the FSM state to Grasshopper
+    fsm_state = "I love Huanyu"
+    print(f"[INFO] Sending FSM state: {fsm_state}")
+    sock_send.sendto(fsm_state.encode("utf-8"), (UDP_IP, UDP_PORT_GH))
+    i += 1 
+
+
